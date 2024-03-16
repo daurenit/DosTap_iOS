cbfe65e31d20ca91f92f50f9faa8e3ccea84ce86
daurenit <dauren.amanbek@nu.edu.kz>
2024-03-17 01:04:29 +0500

    Initial Commit

---
+++ DosTap_iOS/DosTap_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DosTap_iOSApp.swift
+//  DosTap_iOS
+//
+//  Created by Dauren on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DosTap_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
